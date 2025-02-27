04dcc793c14edc06cf23d93180222e464a019944
GustavoAugustoCardoso <gustavo.augusto@payco.com.br>
2025-02-26 22:10:58 -0300

    Chore: fixing docker

--- src/utils/create-pagination-schema.utils.ts
+++ src/utils/create-pagination-schema.utils.ts
@@ -2,11 +2,14 @@ import { type ZodRawShape, z } from 'zod';
 
 import {
   optionalSortSchema,
+  integerNumberSchema,
   optionalPaginationParamSchema,
 } from '../shared/schemas.shared';
 
 const pageSchema = optionalPaginationParamSchema.default(1);
-const limitSchema = optionalPaginationParamSchema.default(10);
+const limitSchema = optionalPaginationParamSchema
+  .default(10)
+  .parse(integerNumberSchema.max(100, 'Max Limit is 100'));
 
 export function createPaginationSchema<T extends ZodRawShape>(fields: T) {
   const paginationSchema = z.object({
